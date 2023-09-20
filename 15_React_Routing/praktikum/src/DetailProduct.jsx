8ff96ed6a9c57e9a88d30d99d2d852e651b43a5b
Hastinul Chotimah <142318032+hastinulchotimah@users.noreply.github.com>
2023-09-20 17:08:13 +0700

    tugas react routing

---
+++ 15_React_Routing/praktikum/src/DetailProduct.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DetailProduct ({product}) {
+    return (
+        <div>
+            <h2>Product Detail</h2>
+            <p><strong>Product Name:</strong>{product.Productname}</p>
+            <p><strong>Product Name:</strong>{product.Productname}</p>
+            <p><strong>Product Name:</strong>{product.Productname}</p>
+            <p><strong>Product Name:</strong>{product.Productname}</p>
+            <p><strong>Product Name:</strong>{product.Productname}</p>
+            <p><strong>Product Name:</strong>{product.Productname}</p>
+
+        </div>
+    )
+}
